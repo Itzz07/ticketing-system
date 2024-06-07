8e08f4be277d2b8d4b44676c8359d118b52dc273
Itzz07 <zimbajoel1@gmail.com>
2024-06-07 16:21:07 +0200

    committed

--- app/api/sendEmail/route.tsx
+++ app/api/sendEmail/route.tsx
@@ -1,16 +1,12 @@
-
 import nodemailer from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { NextResponse, NextRequest } from "next/server";
 
-import { NextResponse } from "next/server";
-
-
-export async function POST(request: { json: () => PromiseLike<{ from: any; to: any; subject: any; text: any; }> | { from: any; to: any; subject: any; text: any; }; }) {
-  const { from, to, subject, text } = await request.json();
-  
-  console.log("Data Recieved:", from);
+export async function POST(request: Request) {
   try {
-    
+    const { from, to, subject, text } = await request.json();
+
+    console.log("Data Received:", from);
 
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
@@ -21,11 +17,6 @@ export async function POST(request: { json: () => PromiseLike<{ from: any; to: a
         pass: process.env.MAIL_PASSWORD,
       },
     } as SMTPTransport.Options);
-
-    // let toAddresses = to;
-    // if (!Array.isArray(to)) {
-    //   toAddresses = [to];
-    // }
 
     const mailOptions = {
       from: from,
@@ -50,6 +41,56 @@ export async function POST(request: { json: () => PromiseLike<{ from: any; to: a
     });
   }
 }
+
+// import nodemailer from "nodemailer";
+// import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+// import { NextResponse, NextRequest } from "next/server";
+
+// export async function POST(request: { json: () => PromiseLike<{ from: any; to: any; subject: any; text: any; }> | { from: any; to: any; subject: any; text: any; }; }) {
+//   const { from, to, subject, text } = await request.json();
+
+//   console.log("Data Recieved:", from);
+//   try {
+
+//     const transporter = nodemailer.createTransport({
+//       host: process.env.MAIL_HOST,
+//       port: Number(process.env.MAIL_PORT),
+//       secure: false,
+//       auth: {
+//         user: process.env.MAIL_USER,
+//         pass: process.env.MAIL_PASSWORD,
+//       },
+//     } as SMTPTransport.Options);
+
+//     // let toAddresses = to;
+//     // if (!Array.isArray(to)) {
+//     //   toAddresses = [to];
+//     // }
+
+//     const mailOptions = {
+//       from: from,
+//       to: to,
+//       subject: subject,
+//       text: text,
+//       html: "<p><b>Hello</b> to myself!</p>",
+//     };
+
+//     await transporter.sendMail(mailOptions);
+//     console.log("Email notification sent");
+//     return NextResponse.json({
+//       status: 200,
+//       message: "Email notification sent successfully.",
+//     });
+//   } catch (error) {
+//     console.error("Error sending email notification:", error);
+//     return NextResponse.json({
+//       status: 500,
+//       message: "Failed to send email notification.",
+//       error: error,
+//     });
+//   }
+// }
 
 // import nodemailer from "nodemailer";
 // import SMTPTransport from "nodemailer/lib/smtp-transport";
